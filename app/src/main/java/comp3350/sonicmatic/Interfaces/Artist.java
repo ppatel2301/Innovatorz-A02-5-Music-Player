f2cab9960adc111551329fe8e2648c8767c52859
Dharm Patel <pateld33@myumanitoba.ca>
2024-02-08 18:56:18 +0000

    Revert "Merge branch 'dev-iteration1-database' into 'main'"
    This reverts merge request !1

--- app/src/main/java/comp3350/sonicmatic/Interfaces/Artist.java
+++
@@ -1,9 +0,0 @@
-package comp3350.sonicmatic.Interfaces;
-
-public interface Artist {
-
-    String getName();
-    void uploadSong();
-    void uploadAlbum();
-
-}
--- app/src/main/java/comp3350/sonicmatic/objects/Library.java
+++
@@ -1,6 +0,0 @@
-package comp3350.sonicmatic.objects;
-
-import comp3350.sonicmatic.Interfaces.MusicLibrary;
-
-public class Library implements MusicLibrary {
-}
