11f2b97569e6e0772f530e9fc17452ae3bd7e1a7
mespn <manuelen2002@gmail.com>
2024-02-08 12:30:53 -0600

    Fixed Unit tests

--- app/src/test/java/comp3350/sonicmatic/DatabaseUnitTest.java
+++ app/src/test/java/comp3350/sonicmatic/DatabaseUnitTest.java
@@ -1,6 +1,8 @@
 package comp3350.sonicmatic;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,25 +11,34 @@ import comp3350.sonicmatic.data.StubDatabase;
 import comp3350.sonicmatic.objects.MusicTrack;
 
 public class DatabaseUnitTest {
-    StubDatabase db = new StubDatabase();
+    private StubDatabase db;
+    private ArrayList<MusicTrack> lst;
 
-    @Test
-    public void uploadSongTest(){
-        MusicTrack track = new MusicTrack();
-        db.uploadSong(track);
+    @Before
+    public void setUp(){
+        db = new StubDatabase();
+        MusicTrack[] tracks ={
+                 new MusicTrack("Elan's file path")
+        };
+        lst = new ArrayList<MusicTrack>(Arrays.asList(tracks));
+        db = new StubDatabase(lst);
     }
 
     @Test
-    public void getSong(){
-        MusicTrack[] tracks ={
-                new MusicTrack(),
-                new MusicTrack(),
-                new MusicTrack(),
-                new MusicTrack()
-        };
-        ArrayList<MusicTrack> lst = new ArrayList<MusicTrack>(Arrays.asList(tracks));
-        db = new StubDatabase(lst);
+    public void uploadSongTest(){
+        MusicTrack track = new MusicTrack("Other file path");
+        db.uploadSong(track);
+    }
 
+
+
+    @Test
+    public void getSong(){
+        MusicTrack first = db.getSong("First song's name");
+        MusicTrack failure = db.getSong("Let It Go - From Disney's Frozen");
+
+        assertEquals(lst.get(0), first);
+        assertNull(failure);
     }
 
 }
