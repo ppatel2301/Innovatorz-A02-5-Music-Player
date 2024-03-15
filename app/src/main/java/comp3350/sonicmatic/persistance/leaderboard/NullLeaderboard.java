a62167885bfba6bc8a6797713f8aaa7d8e450494
mespn <manuelen2002@gmail.com>
2024-03-15 14:31:57 -0500

    Fixed error

--- app/src/main/java/comp3350/sonicmatic/persistance/leaderboard/NullLeaderboard.java
+++ app/src/main/java/comp3350/sonicmatic/persistance/leaderboard/NullLeaderboard.java
@@ -3,9 +3,10 @@ package comp3350.sonicmatic.persistance.leaderboard;
 import java.util.ArrayList;
 
 import comp3350.sonicmatic.interfaces.IArtist;
+import comp3350.sonicmatic.objects.LeaderboardArtist;
 
 public class NullLeaderboard extends Leaderboard{
-    private static final ArrayList<IArtist> emptyLeaderboard = new ArrayList<>();
+    private static final ArrayList<LeaderboardArtist> emptyLeaderboard = new ArrayList<>();
     private static NullLeaderboard nullLeaderboard = null;
 
     public static NullLeaderboard getNullLeaderboard(){
