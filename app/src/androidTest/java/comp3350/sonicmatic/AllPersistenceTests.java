84592691cba8e33f3951eacd81d9331e160fd70c
Elan <elan.gabor@gmail.com>
2024-03-27 20:11:38 -0500

    Renaming and some testing changes

---
+++ app/src/androidTest/java/comp3350/sonicmatic/AllPersistenceTests.java
@@ -0,0 +1,20 @@
+package comp3350.sonicmatic;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+import comp3350.sonicmatic.database.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ProfilePersistenceTest.class,
+        SongPersistenceTest.class,
+        PlaylistSongPersistenceTest.class,
+        PlaylistPersistenceTest.class
+})
+
+public class AllPersistenceTests
+{
+
+}
