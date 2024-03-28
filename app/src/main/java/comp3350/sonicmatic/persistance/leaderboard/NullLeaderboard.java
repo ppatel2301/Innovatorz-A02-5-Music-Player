fde54d27bb8bd6e61fb2a4edfafafabdfb22d084
Elan <elan.gabor@gmail.com>
2024-03-28 14:00:21 -0500

    NullLeaderboard can handle Services not having context

--- app/src/main/java/comp3350/sonicmatic/persistance/leaderboard/NullLeaderboard.java
+++ app/src/main/java/comp3350/sonicmatic/persistance/leaderboard/NullLeaderboard.java
@@ -3,6 +3,7 @@ package comp3350.sonicmatic.persistance.leaderboard;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import comp3350.sonicmatic.application.Services;
 import comp3350.sonicmatic.business.access.AccessSong;
 import comp3350.sonicmatic.interfaces.ISong;
 import comp3350.sonicmatic.objects.musicArtist.LeaderboardArtist;
@@ -23,12 +24,24 @@ public class NullLeaderboard extends Leaderboard{
     }
 
     private NullLeaderboard(){
-        AccessSong accessSong = new AccessSong();
-        ArrayList<ISong> allSongs = accessSong.getAllSongs();
-        for (ISong song: allSongs){
-            emptyLeaderboard.add(new LeaderboardArtist(song.getArtist(), 0));
+        AccessSong accessSong;
+        ArrayList<ISong> allSongs;
+
+        if (Services.hasContext())
+        {
+            accessSong = new AccessSong();
+            allSongs = accessSong.getAllSongs();
+
+            for (ISong song: allSongs){
+                emptyLeaderboard.add(new LeaderboardArtist(song.getArtist(), 0));
+            }
+
+            this.board = new ArrayList<>(emptyLeaderboard);
+        }
+        else
+        {
+            this.board = new ArrayList<>();
         }
 
-        this.board = new ArrayList<>(emptyLeaderboard);
     }
 }
